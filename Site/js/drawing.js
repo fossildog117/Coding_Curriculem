9ba366e81bdd0c53cf2075213ea72a1e79a028ef
HaleAT <ahale@openmailbox.org>
2016-02-11 14:34:44 +0000

    Provide javascript for drawing exercises
    Adds necessary functions to manipulate canvas for drawing exercises

---
+++ Site/js/drawing.js
@@ -0,0 +1,22 @@
+function drawLine(x1,y1,x2,y2)
+{
+    var c = document.getElementById("drawing");
+    var ctx = c.getContext("2d");
+    ctx.moveTo(x1,y1);
+    ctx.lineTo(x2,y2);
+    ctx.stroke();
+}
+
+function fillRect(x,y,width,height)
+{
+    var c = document.getElementById("drawing");
+    var ctx = c.getContext("2d");
+    ctx.fillRect(x,y,width,height);
+}
+
+function clearCanvas()
+{
+    var c = document.getElementById("drawing");
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, 400, 400);
+}
