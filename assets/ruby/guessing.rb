6c665c6eded914dda719ccf969c23e8dfd8d5272
Alex Hale <ahale@openmailbox.org>
2016-03-24 00:59:32 +0000

    Remove debug code and update ruby file

--- assets/ruby/guessing.rb
+++ assets/ruby/guessing.rb
@@ -3,7 +3,15 @@ class Game
         @guessing = Guessing.new
     end
 
-    def guess
-        @guessing.guess(guess)
+    def guess(attempt)
+        result = @guessing.guess(attempt)
+        if (result == 0)
+            %x{ document.getElementById('output').innerHTML = "Correct!";
+            }
+        elsif (result == -1)
+            %x{ document.getElementById('output').innerHTML = "Too low!"; }
+        else
+            %x{ document.getElementById('output').innerHTML = "Too high!"; }
+        end
     end
 end
